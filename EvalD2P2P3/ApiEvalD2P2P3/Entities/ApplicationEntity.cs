57337287a0a55945b114a65eb331fc0a3944512c
Tristan DAPOIGNY <tristan.dapoigny@diiage.org>
2025-02-26 10:16:19 +0100

    Création du projet et ajout des entities

---
+++ EvalD2P2P3/ApiEvalD2P2P3/Entities/ApplicationEntity.cs
@@ -0,0 +1,18 @@
+﻿namespace ApiEvalD2P2P3.Entities
+{
+    public class ApplicationEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public ApplicationType Type { get; set; }
+
+        public ICollection<PasswordEntity> Passwords { get; set; } = new List<PasswordEntity>();
+    }
+
+    public enum ApplicationType
+    {
+        GrandPublic,  // AES
+        Professionnelle // RSA
+    }
+
+}
---
+++ EvalD2P2P3/ApiEvalD2P2P3/Entities/PasswordEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiEvalD2P2P3.Entities
+{
+    public class PasswordEntity
+    {
+        public int Id { get; set; }
+        public string EncryptedPassword { get; set; } = string.Empty;
+        public string? EncryptionKey { get; set; } // Clé privée pour RSA, null pour AES
+        public int ApplicationId { get; set; }
+
+        public ApplicationEntity Application { get; set; }
+    }
+
+}
